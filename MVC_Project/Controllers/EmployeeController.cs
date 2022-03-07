afad9bf5fddbdf5c84775d7a500a16fb01de1155
ROY <ROY@DESKTOP-26H23PD>
2022-03-07 13:00:46 +0530

    Add project files.

---
+++ MVC_Project/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_Project.Controllers;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Employee> employees = dbContext.Employees.ToList();
+            return View(employees);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            dbContext.Employees.Add(emp);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
